b6ec6fce7f7a9fc336b60f8fb32d2ffa6ed0847d
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-03-25 13:41:29 +0200

    Replace @Data on BaseEntity with @EqualsAndHashCode

--- src/main/java/com/ahmedsameha1/todo/domain_model/BaseEntity.java
+++ src/main/java/com/ahmedsameha1/todo/domain_model/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.ahmedsameha1.todo.domain_model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
@@ -10,7 +12,7 @@ import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
 @MappedSuperclass
-@Data
+@EqualsAndHashCode
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -19,5 +21,6 @@ public abstract class BaseEntity {
             strategy = "org.hibernate.id.UUIDGenerator"
     )
     @Column(name = "id", updatable = false, nullable = false)
+    @Getter
     private UUID id;
 }
