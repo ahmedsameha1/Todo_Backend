39d81439dbeb6dc5e9480258fef09f9777a33689
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-03-28 17:13:16 +0200

    Add validation rules for Todo entity

--- src/main/java/com/ahmedsameha1/todo/domain_model/Todo.java
+++ src/main/java/com/ahmedsameha1/todo/domain_model/Todo.java
@@ -3,19 +3,34 @@ package com.ahmedsameha1.todo.domain_model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Todo extends BaseEntity {
+    @NotBlank
+    @Size(min = 1, max = 200000000)
+    @Column(nullable = false)
     private String description;
+
+    @Future
+    @Column(nullable = false)
     private LocalDate targetDate;
-    private boolean isDone;
+
+    @NotNull
+    @Column(nullable = false)
+    private boolean isDone = false;
+
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- src/main/java/com/ahmedsameha1/todo/domain_model/User.java
+++ src/main/java/com/ahmedsameha1/todo/domain_model/User.java
@@ -22,42 +22,53 @@ public class User extends BaseEntity implements UserDetails {
     @Size(min = 1, max = 50)
     @Column(unique = true, nullable = false)
     private String username;
+
     @NotBlank
     @Size(min = 8, max = 255)
     @Column(nullable = false)
     private String password;
+
     @NotBlank
     @Size(min = 1, max = 100)
     @Column(nullable = false)
     private String firstName;
+
     @NotBlank
     @Size(min = 1, max = 100)
     @Column(nullable = false)
     private String lastName;
+
     @NotBlank
     @Size(min = 5, max = 100)
     @Email
     @Column(nullable = false)
     private String email;
+
     @NotNull
     @Past
     @Column(nullable = false)
     private LocalDate birthDay;
+
     @NotNull
     @Column(nullable = false)
     private Gender gender = Gender.UNSPECIFIED;
+
     @NotNull
     @OneToMany(mappedBy = "user")
     private List<Todo> todos = Collections.emptyList();
+
     @NotNull
     @Column(nullable = false)
     private boolean enabled = false;
+
     @NotNull
     @Column(nullable = false)
     private boolean locked = false;
+
     @NotNull
     @Column(nullable = false)
     private boolean expired = false;
+
     @NotNull
     @Column(nullable = false)
     private boolean credentialsExpired = false;
