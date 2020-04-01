dad271d75f4a4c4a1a25b138040dc5e425c64b12
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-01 12:01:51 +0200

    Fix errors in the user_account table schema

--- src/main/resources/sc.sql
+++ src/main/resources/sc.sql
@@ -5,35 +5,35 @@ create table user_account (
         and
         char_length(username) <= 50
         and
-        array_length(regexp_matches(username, '^.*\S.*$')) >= 1
+        username ~ '^.*\S.*$'
     ),
     password varchar(255) not null check (
         char_length(username) >= 8
         and
         char_length(username) <= 255
         and
-        array_length(regexp_matches(password, '^.*\S.*$')) >= 1
+        password ~ '^.*\S.*$'
     ),
     first_name varchar(100) not null check (
         char_length(username) >= 1
         and
         char_length(username) <= 100
         and
-        array_length(regexp_matches(first_name, '^.*\S.*$')) >= 1
+        first_name ~ '^.*\S.*$'
     ),
     last_name varchar(100) not null check (
         char_length(username) >= 1
         and
         char_length(username) <= 100
         and
-        array_length(regexp_matches(last_name, '^.*\S.*$')) >= 1
+        last_name ~ '^.*\S.*$'
     ),
     email varchar(255) not null check(
         char_length(username) >= 5
         and
         char_length(username) <= 255
         and
-        array_length(regexp_matches(email, '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')) = 1
+        email ~* '^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$'
     ),
     birth_day date not null check(birth_day < current_date),
     gender varchar(50) default 'UNSPECIFIED',
@@ -41,5 +41,5 @@ create table user_account (
     locked boolean default false,
     expired boolean default false,
     credentials_expired boolean default false,
-    version bigserial default 0
+    version bigint default 0
 );
