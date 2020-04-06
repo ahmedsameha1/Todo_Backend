0081db655ba79083f7b75d4e5bd4dec4c7a85993
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-06 16:40:25 +0200

    Implement successfulAuthentication() method in JwtAuthenticationFilter class

--- src/main/java/com/ahmedsameha1/todo/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ahmedsameha1/todo/security/filter/JwtAuthenticationFilter.java
@@ -2,20 +2,37 @@ package com.ahmedsameha1.todo.security.filter;
 
 import com.ahmedsameha1.todo.domain_model.UserAccount;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+import static com.ahmedsameha1.todo.security.Constants.*;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
@@ -29,5 +46,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String jws = Jwts.builder()
+                .setSubject(((UserDetails) authResult.getPrincipal()).getUsername())
+                .setExpiration(Timestamp.valueOf(LocalDateTime.now(Clock.systemUTC()).plusDays(EXPIRATION_PERIOD_IN_DAYS)))
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey))).compact();
+        response.addHeader(AUTHORIZATION, TOKEN_PREFIX + jws);
     }
 }
