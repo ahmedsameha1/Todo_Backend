292f68c21abf55b9a277632cc40e509d42b151cb
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-06 16:36:10 +0200

    Create Constants class to put security constants

---
+++ src/main/java/com/ahmedsameha1/todo/security/Constants.java
@@ -0,0 +1,8 @@
+package com.ahmedsameha1.todo.security;
+
+public class Constants {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String SIGN_UP_URL = "/sign_up";
+    public static final byte EXPIRATION_PERIOD_IN_DAYS = 10;
+}
