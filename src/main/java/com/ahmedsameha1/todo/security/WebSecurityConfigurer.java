a6e3d65c8e8036609f7199a78fd1a49309736a7f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-07 16:49:50 +0200

    Override configure(HttpSecurity) method in WebSecurityConfigurer class to add our filters and to handle requests security

--- src/main/java/com/ahmedsameha1/todo/security/WebSecurityConfigurer.java
+++ src/main/java/com/ahmedsameha1/todo/security/WebSecurityConfigurer.java
@@ -1,14 +1,22 @@
 package com.ahmedsameha1.todo.security;
 
+import com.ahmedsameha1.todo.security.filter.JwtAuthenticationFilter;
+import com.ahmedsameha1.todo.security.filter.JwtAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static com.ahmedsameha1.todo.security.Constants.SIGN_UP_URL;
 
 @EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -17,6 +25,23 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and()
+                // This because we use JWT
+                .csrf().disable()
+                /////////////////////////
+                .authorizeRequests()
+                // This to allow any unknown user to sign up
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                ////////////////////////////////////////////////
+                .anyRequest().authenticated().and()
+                .addFilter(new JwtAuthenticationFilter())
+                .addFilter(new JwtAuthorizationFilter(authenticationManager()))
+                // This because we don't use sessions
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
