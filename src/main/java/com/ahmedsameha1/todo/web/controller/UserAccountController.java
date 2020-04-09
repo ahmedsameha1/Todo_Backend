58d93bcb7e6ea67d637072990707b1eff531d138
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-09 18:15:53 +0200

    Change signUp endpoint mehtod of UserAccountController to use UserAccountService instead of UserAccountRepository

--- src/main/java/com/ahmedsameha1/todo/web/controller/UserAccountController.java
+++ src/main/java/com/ahmedsameha1/todo/web/controller/UserAccountController.java
@@ -1,10 +1,9 @@
 package com.ahmedsameha1.todo.web.controller;
 
 import com.ahmedsameha1.todo.domain_model.UserAccount;
-import com.ahmedsameha1.todo.repository.UserAccountRepository;
+import com.ahmedsameha1.todo.service.UserAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +15,11 @@ import static com.ahmedsameha1.todo.security.Constants.SIGN_UP_URL;
 @RestController
 public class UserAccountController {
     @Autowired
-    private UserAccountRepository userAccountRepository;
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserAccountService userAccountService;
 
     @PostMapping(SIGN_UP_URL)
     public ResponseEntity<?> signUp(@RequestBody @Valid UserAccount userAccount) {
-        userAccount.setPassword(bCryptPasswordEncoder.encode(userAccount.getPassword()));
-        userAccountRepository.save(userAccount);
+        userAccountService.registerNewUserAccount(userAccount);
         return ResponseEntity.ok().build();
     }
 }
