512eb284c9d9738eb47dd0d06de5221712751e3b
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-09 17:16:34 +0200

    Create UserAccountServiceImpl that implements UserAccountService

--- src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
@@ -2,9 +2,7 @@ package com.ahmedsameha1.todo.service;
 
 import com.ahmedsameha1.todo.domain_model.UserAccount;
 import com.ahmedsameha1.todo.exception.UserExistsException;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserAccountService {
     UserAccount registerNewUserAccount(UserAccount userAccount) throws UserExistsException;
 }
---
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountServiceImpl.java
@@ -0,0 +1,7 @@
+package com.ahmedsameha1.todo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+}
