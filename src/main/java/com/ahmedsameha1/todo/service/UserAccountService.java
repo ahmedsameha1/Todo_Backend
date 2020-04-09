30fc7bdada31279a2ae838d63d7342bb2fb7b941
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-09 17:10:35 +0200

    Create registerNewUserAccount() method in UserAccountService to handle UserAccount sign up

--- src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface UserAccountService {
+    UserAccount registerNewUserAccount(UserAccount userAccount) throws UserExistsException;
 }
