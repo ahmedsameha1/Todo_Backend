00ba1494ed825e2bd530aaea7b4310bd40178b0c
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-09 17:08:14 +0200

    Create UserAccountService interface to handle UserAccount operations

---
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
@@ -0,0 +1,9 @@
+package com.ahmedsameha1.todo.service;
+
+import com.ahmedsameha1.todo.domain_model.UserAccount;
+import com.ahmedsameha1.todo.exception.UserExistsException;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserAccountService {
+}
