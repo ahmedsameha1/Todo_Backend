d446800fdd67ec6e681d135b738c2d803ba93a93
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-17 05:56:43 +0200

    Do not include empty fields when serialize ErrorResponse to json

--- src/main/java/com/ahmedsameha1/todo/exception/ErrorResponse.java
+++ src/main/java/com/ahmedsameha1/todo/exception/ErrorResponse.java
@@ -1,11 +1,13 @@
 package com.ahmedsameha1.todo.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Setter @Getter
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ErrorResponse {
     private Instant timestamp = Instant.now();
     private String path;
