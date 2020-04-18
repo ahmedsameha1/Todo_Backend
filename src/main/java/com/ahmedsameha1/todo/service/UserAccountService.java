1d6669500b78f84078a6a234aa8a927f1f10e689
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-18 14:10:24 +0200

    Add authenticate() method to UserAccountService then implement it in UserAccountServiceImpl

--- src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountService.java
@@ -3,10 +3,11 @@ package com.ahmedsameha1.todo.service;
 import com.ahmedsameha1.todo.domain_model.EmailVerificationToken;
 import com.ahmedsameha1.todo.domain_model.UserAccount;
 import com.ahmedsameha1.todo.exception.UserExistsException;
+import com.ahmedsameha1.todo.security.SignInRequest;
 
 public interface UserAccountService {
     UserAccount registerNewUserAccount(UserAccount userAccount) throws UserExistsException;
     EmailVerificationToken createEmailVerificationToken(UserAccount userAccount);
-
     void enableUserAccount(String token);
+    String authenticate(SignInRequest signInRequest);
 }
--- src/main/java/com/ahmedsameha1/todo/service/UserAccountServiceImpl.java
+++ src/main/java/com/ahmedsameha1/todo/service/UserAccountServiceImpl.java
@@ -3,19 +3,32 @@ package com.ahmedsameha1.todo.service;
 import com.ahmedsameha1.todo.domain_model.EmailVerificationToken;
 import com.ahmedsameha1.todo.domain_model.UserAccount;
 import com.ahmedsameha1.todo.exception.BadEmailVerificationTokenException;
+import com.ahmedsameha1.todo.exception.ExpiredEmailVerificationTokenException;
 import com.ahmedsameha1.todo.exception.UserExistsException;
 import com.ahmedsameha1.todo.repository.EmailVerificationTokenRepository;
 import com.ahmedsameha1.todo.repository.UserAccountRepository;
+import com.ahmedsameha1.todo.security.SignInRequest;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static com.ahmedsameha1.todo.security.Constants.EMAIL_VERIFICATION_TOKEN_EXPIRATION_PERIOD_IN_DAYS;
+import static com.ahmedsameha1.todo.security.Constants.JWT_TOKEN_EXPIRATION_PERIOD_IN_DAYS;
 
 @Service
 public class UserAccountServiceImpl implements UserAccountService {
@@ -27,6 +40,13 @@ public class UserAccountServiceImpl implements UserAccountService {
 
     @Autowired
     private EmailVerificationTokenRepository emailVerificationTokenRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Value("${jwtSecret}")
+    private String jwtSecret;
+
     @Override
     public UserAccount registerNewUserAccount(UserAccount userAccount) throws UserExistsException {
         if (userAccountRepository.findByUsername(userAccount.getUsername()) != null) {
@@ -60,5 +80,27 @@ public class UserAccountServiceImpl implements UserAccountService {
         userAccount.setEnabled(true);
         userAccountRepository.save(userAccount);
         emailVerificationTokenRepository.delete(emailVerificationToken);
+    }
+
+    @Override
+    public String authenticate(SignInRequest signInRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(signInRequest.getUsername(),
+                            signInRequest.getPassword()));
+        } catch (DisabledException de) {
+           var userAccount = userAccountRepository.findByUsername(signInRequest.getUsername());
+           var emailVerificationToken = emailVerificationTokenRepository.findByUserAccount(userAccount);
+           if (emailVerificationToken.getExpiresAt().isBefore(LocalDateTime.now(Clock.systemUTC()))) {
+               throw new ExpiredEmailVerificationTokenException();
+           } else {
+               throw de;
+           }
+        }
+        return  Jwts.builder()
+                .setSubject(((UserDetails) authentication.getPrincipal()).getUsername())
+                .setExpiration(Timestamp.valueOf(LocalDateTime.now(Clock.systemUTC()).plusDays(JWT_TOKEN_EXPIRATION_PERIOD_IN_DAYS)))
+                .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret))).compact();
     }
 }
