6769abce3a63df6b5f6deae17c96bf319feb1887
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-18 14:06:17 +0200

    Add findByUserAccount() to EmailVerificationTokenRepository class

--- src/main/java/com/ahmedsameha1/todo/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/ahmedsameha1/todo/repository/EmailVerificationTokenRepository.java
@@ -1,10 +1,12 @@
 package com.ahmedsameha1.todo.repository;
 
 import com.ahmedsameha1.todo.domain_model.EmailVerificationToken;
+import com.ahmedsameha1.todo.domain_model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, UUID> {
     EmailVerificationToken findByToken(String token);
+    EmailVerificationToken findByUserAccount(UserAccount userAccount);
 }
