4b158eb77d31332e06f8b82d0df926e3351150d8
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-18 15:25:59 +0200

    Create ErrorCode inner class in Constants class & add BAD_EMAIL_VERIFICATION_TOKEN to it

--- src/main/java/com/ahmedsameha1/todo/security/Constants.java
+++ src/main/java/com/ahmedsameha1/todo/security/Constants.java
@@ -8,4 +8,8 @@ public class Constants {
     public static final String SIGN_IN_URL = "/sign_in";
     public static final byte JWT_TOKEN_EXPIRATION_PERIOD_IN_DAYS = 10;
     public static final byte EMAIL_VERIFICATION_TOKEN_EXPIRATION_PERIOD_IN_DAYS = 1;
+
+    public static class ErrorCode {
+        public static final short BAD_EMAIL_VERIFICATION_TOKEN = 1;
+    }
 }
