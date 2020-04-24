b3002ce312b7b10e362a0c70bd13635b28f72d7f
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-24 20:26:27 +0200

    Add creationTime and updateTime to BaseEntity

--- src/main/java/com/ahmedsameha1/todo/domain_model/BaseEntity.java
+++ src/main/java/com/ahmedsameha1/todo/domain_model/BaseEntity.java
@@ -3,16 +3,21 @@ package com.ahmedsameha1.todo.domain_model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
 @EqualsAndHashCode
+@Getter
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(generator = "UUID")
@@ -21,7 +26,12 @@ public abstract class BaseEntity {
             strategy = "org.hibernate.id.UUIDGenerator"
     )
     @Column(name = "id", updatable = false, nullable = false)
-    @Getter
     @JsonIgnore
     private UUID id;
+
+    @CreationTimestamp
+    private Instant creationTime;
+
+    @UpdateTimestamp
+    private Instant updateTime;
 }
