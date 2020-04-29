d11cf2a655664f2cfe962792e6d94f5ac6d4b166
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-04-29 13:22:09 +0200

    Add the following test cases to UserAccountRepositoryTest: Should fail because username is null Should fail because username is an empty string Should fail because username is a string that only contains whitespace

--- src/test/java/com/ahmedsameha1/todo/domain_model/UserAccountRepositoryTest.java
+++ src/test/java/com/ahmedsameha1/todo/domain_model/UserAccountRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.ahmedsameha1.todo.domain_model;
 
 import com.ahmedsameha1.todo.repository.UserAccountRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.TransactionSystemException;
 
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserAccountRepositoryTest extends ProductionDatabaseBaseTest {
     @Autowired
@@ -39,7 +42,7 @@ public class UserAccountRepositoryTest extends ProductionDatabaseBaseTest {
     public void test1() {
         userAccountRepository.save(userAccount);
         var databaseUserAccount = userAccountRepository.findByUsername(randomUserName);
-        Assertions.assertThat(databaseUserAccount).isNotNull();
+        assertThat(databaseUserAccount).isNotNull();
     }
 
     @Test
@@ -47,7 +50,7 @@ public class UserAccountRepositoryTest extends ProductionDatabaseBaseTest {
     public void test2() {
         userAccountRepository.save(userAccount);
         var databaseUserAccount = userAccountRepository.findByUsername(randomUserName);
-        Assertions.assertThat(databaseUserAccount.getId()).isNotNull();
+        assertThat(databaseUserAccount.getId()).isNotNull();
     }
 
     @Test
@@ -55,13 +58,58 @@ public class UserAccountRepositoryTest extends ProductionDatabaseBaseTest {
     public void test3() {
         userAccountRepository.save(userAccount);
         var databaseUserAccount = userAccountRepository.findByUsername(randomUserName);
-        Assertions.assertThat(databaseUserAccount.getUsername()).isEqualTo(randomUserName);
-        Assertions.assertThat(databaseUserAccount.getPassword()).isEqualTo("ffffffff");
-        Assertions.assertThat(databaseUserAccount.getFirstName()).isEqualTo("user2");
-        Assertions.assertThat(databaseUserAccount.getLastName()).isEqualTo("user2");
-        Assertions.assertThat(databaseUserAccount.getGender()).isEqualTo(Gender.MALE);
-        Assertions.assertThat(databaseUserAccount.getBirthDay())
+        assertThat(databaseUserAccount.getUsername()).isEqualTo(randomUserName);
+        assertThat(databaseUserAccount.getPassword()).isEqualTo("ffffffff");
+        assertThat(databaseUserAccount.getFirstName()).isEqualTo("user2");
+        assertThat(databaseUserAccount.getLastName()).isEqualTo("user2");
+        assertThat(databaseUserAccount.getGender()).isEqualTo(Gender.MALE);
+        assertThat(databaseUserAccount.getBirthDay())
                 .isEqualTo(LocalDate.of(2010, 10, 10));
-        Assertions.assertThat(databaseUserAccount.getEmail()).isEqualTo("user2@user2.com");
+        assertThat(databaseUserAccount.getEmail()).isEqualTo("user2@user2.com");
+    }
+
+    @Test
+    @DisplayName("Should fail because username is null")
+    public void test4() {
+        userAccount.setUsername(null);
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    @DisplayName("Should fail because username is an empty string")
+    public void test5() {
+        userAccount.setUsername("");
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    @DisplayName("Should fail because username is a string that only contains whitespace")
+    public void test6() {
+        userAccount.setUsername("  ");
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        userAccount.setUsername("\n");
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        userAccount.setUsername("\r");
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        userAccount.setUsername("\t");
+        assertThatThrownBy(() -> userAccountRepository.save(userAccount))
+                .isInstanceOf(TransactionSystemException.class)
+                .hasCauseInstanceOf(RollbackException.class)
+                .hasRootCauseInstanceOf(ConstraintViolationException.class);
     }
 }
