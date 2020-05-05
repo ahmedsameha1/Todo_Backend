aa80cd0cc738ac15e8b8cc3c7e9fe9ab3c9e255a
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-05-05 11:31:58 +0200

    Add test case to check the default value of gender field of UserAccount

--- src/test/java/com/ahmedsameha1/todo/domain_model/UserAccountTest.java
+++ src/test/java/com/ahmedsameha1/todo/domain_model/UserAccountTest.java
@@ -1,6 +1,6 @@
 package com.ahmedsameha1.todo.domain_model;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,49 +14,54 @@ public class UserAccountTest {
 
     @Test
     public void testIsEnabled() {
-        Assertions.assertThat(userAccount.isEnabled()).isFalse();
+        assertThat(userAccount.isEnabled()).isFalse();
         userAccount.setEnabled(true);
-        Assertions.assertThat(userAccount.isEnabled()).isTrue();
+        assertThat(userAccount.isEnabled()).isTrue();
         userAccount.setEnabled(false);
-        Assertions.assertThat(userAccount.isEnabled()).isFalse();
+        assertThat(userAccount.isEnabled()).isFalse();
     }
 
     @Test
     public void testIsAccountNonLocked() {
-        Assertions.assertThat(userAccount.isAccountNonLocked()).isTrue();
+        assertThat(userAccount.isAccountNonLocked()).isTrue();
         userAccount.setLocked(true);
-        Assertions.assertThat(userAccount.isAccountNonLocked()).isFalse();
+        assertThat(userAccount.isAccountNonLocked()).isFalse();
         userAccount.setLocked(false);
-        Assertions.assertThat(userAccount.isAccountNonLocked()).isTrue();
+        assertThat(userAccount.isAccountNonLocked()).isTrue();
     }
 
     @Test
     public void testIsAccountNonExpired() {
-        Assertions.assertThat(userAccount.isAccountNonExpired()).isTrue();
+        assertThat(userAccount.isAccountNonExpired()).isTrue();
         userAccount.setExpired(true);
-        Assertions.assertThat(userAccount.isAccountNonExpired()).isFalse();
+        assertThat(userAccount.isAccountNonExpired()).isFalse();
         userAccount.setExpired(false);
-        Assertions.assertThat(userAccount.isAccountNonExpired()).isTrue();
+        assertThat(userAccount.isAccountNonExpired()).isTrue();
     }
 
     @Test
     public void testIsCredentialsNonExpired() {
-        Assertions.assertThat(userAccount.isCredentialsNonExpired()).isTrue();
+        assertThat(userAccount.isCredentialsNonExpired()).isTrue();
         userAccount.setCredentialsExpired(true);
-        Assertions.assertThat(userAccount.isCredentialsNonExpired()).isFalse();
+        assertThat(userAccount.isCredentialsNonExpired()).isFalse();
         userAccount.setCredentialsExpired(false);
-        Assertions.assertThat(userAccount.isCredentialsNonExpired()).isTrue();
+        assertThat(userAccount.isCredentialsNonExpired()).isTrue();
     }
 
     @Test
     public void testTodosPopulatedWithEmptyList() {
-        Assertions.assertThat(userAccount.getTodos()).isNotNull();
-        Assertions.assertThat(userAccount.getTodos()).isEmpty();
+        assertThat(userAccount.getTodos()).isNotNull();
+        assertThat(userAccount.getTodos()).isEmpty();
     }
 
     @Test
     public void testAuthoritiesPopulatedWithEmptyList() {
-        Assertions.assertThat(userAccount.getAuthorities()).isNotNull();
-        Assertions.assertThat(userAccount.getAuthorities()).isEmpty();
+        assertThat(userAccount.getAuthorities()).isNotNull();
+        assertThat(userAccount.getAuthorities()).isEmpty();
+    }
+
+    @Test
+    public void testDefaultValueOfGender() {
+        assertThat(userAccount.getGender()).isEqualTo(Gender.UNSPECIFIED);
     }
 }
