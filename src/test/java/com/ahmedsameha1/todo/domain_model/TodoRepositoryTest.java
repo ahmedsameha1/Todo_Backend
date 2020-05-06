3196776f9bfb68935551554cd60acc299f50cb53
Ahmed Mohamed Sameh <ahmedsameha1@gmail.com>
2020-05-06 05:35:33 +0200

    Add Description inner class to TodoRepositoryTest that contains test cases of description field of Todo

--- src/test/java/com/ahmedsameha1/todo/domain_model/TodoRepositoryTest.java
+++ src/test/java/com/ahmedsameha1/todo/domain_model/TodoRepositoryTest.java
@@ -8,10 +8,12 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class TodoRepositoryTest extends ProductionDatabaseBaseTest {
     @Autowired
@@ -61,6 +63,57 @@ public class TodoRepositoryTest extends ProductionDatabaseBaseTest {
             assertThat(databaseTodo.getTargetDate()).isEqualTo(LocalDate.of(2025, 10, 10));
             assertThat(databaseTodo.getUserAccount()).isEqualTo(userAccount);
             assertThat(databaseTodo.isDone()).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("Description tests")
+    class Description {
+        @Test
+        @DisplayName("Should fail because description is null")
+        public void test1() {
+            todo.setDescription(null);
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        @DisplayName("Should fail because description is an empty string")
+        public void test2() {
+            todo.setDescription("");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        @DisplayName("Should fail because description has only whitespace")
+        public void test3() {
+            todo.setDescription(" ");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+            todo.setDescription("\t");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+            todo.setDescription("\n");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+            todo.setDescription("\r");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+            todo.setDescription("\t\n");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+            todo.setDescription("\r ");
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
+        }
+
+        @Test
+        @DisplayName("Should fail because the length of description is larger than 200000000")
+        public void test4() {
+            todo.setDescription("f".repeat(200000001));
+            assertThatThrownBy(() -> todoRepository.save(todo))
+                    .hasRootCauseInstanceOf(ConstraintViolationException.class);
         }
     }
 }
